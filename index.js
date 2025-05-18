71eb99a338252521f672d1f41b988ca938b98257
patriciauhh <patriciaa@patricias-mbp-2.mynetworksettings.com>
2025-05-18 17:52:52 -0400

    pls work

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(express.json());
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
+app.get('/', (req, res) => {
+  res.sendFile('public/INST377-Week12-Customers.html', { root: __dirname });
+});
+
 // POST /api/save-playlist
 app.post('/save-playlist', async (req, res) => {
   const { genre, playlist } = req.body;
